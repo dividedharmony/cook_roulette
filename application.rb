3f13ff55e0d608f1401f16d17ca76803e7e1c2b5
David Harmon <dharmon202@gmail.com>
2021-03-17 22:58:29 -0400

    Load bundler according to the SINATRA_ENV env variable
    This env variable is determined by the .env file but that file
    should not be present in production so we load it conditionally.

--- application.rb
+++ application.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
-require 'sinatra'
+SINATRA_ENV = ENV.fetch("SINATRA_ENV") do |el|
+  require 'dotenv/load'
+  ENV.fetch(el)
+end.to_sym
+
+require 'bundler'
+
+Bundler.require(:default, SINATRA_ENV)
 
 get '/' do
   erb :"form.html"
