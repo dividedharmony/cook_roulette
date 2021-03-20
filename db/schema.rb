d82ae82a51fca2c0aad14528bd3f23f7cda7826b
David Harmon <dharmon202@gmail.com>
2021-03-20 12:25:18 -0400

    Add ingredients table
    This table will store ingredients that will be used across
    multiple recipes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_101307) do
+ActiveRecord::Schema.define(version: 2021_03_20_122328) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "url", null: false
